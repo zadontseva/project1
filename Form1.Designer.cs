75a76150c9cde93f41d1e2cc7dbdb0db6a23dfec
Valeria <Valeria@DESKTOP-G99I8BG>
2019-02-22 21:32:13 +0200

    Add project

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(22, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(277, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(277, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(277, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 253);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(277, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Дата народження:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вік:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Знак Зодіаку:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Китайський знак Зодіаку:";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 355);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "BIrth App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Enabled = false;
+            var count = await Task<int>.Factory.StartNew(()=>
+            {
+                int year = (DateTime.Now.Date.Year - dateTimePicker1.Value.Date.Year);
+                return year;
+            });
+            
+
+            
+
+            if (count > -1 && count < 135)
+                textBox1.Text = count.ToString();
+            else
+            {
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                string message = "Необхідно ввести вік, що не перевищує 135 років";
+                string caption = "Помилка вводу віку";
+                DialogResult result = MessageBox.Show(message, caption, button);
+                if (result == System.Windows.Forms.DialogResult.OK)
+                    dateTimePicker1.Value = DateTime.Now.AddDays(-1);
+            }
+
+            var zodiac1 = await Task<string>.Factory.StartNew(() =>
+            {
+                int day = dateTimePicker1.Value.Day;
+                int month = dateTimePicker1.Value.Month;
+                string sign;
+                switch (month)
+                {
+                    case 1: sign = (day <= 20) ? "Козеріг" : "Водолій"; break;
+                    case 2: sign = (day <= 19) ? "Водолій" : "Риби"; break;
+                    case 3: sign = (day <= 20) ? "Риби" : "Овен"; break;
+                    case 4: sign = (day <= 20) ? "Овен" : "Телець"; break;
+                    case 5: sign = (day <= 21) ? "Телець" : "Близнюки"; break;
+                    case 6: sign = (day <= 21) ? "Близнюки" : "Рак"; break;
+                    case 7: sign = (day <= 22) ? "Рак" : "Лев"; break;
+                    case 8: sign = (day <= 23) ? "Лев" : "Діва"; break;
+                    case 9: sign = (day <= 23) ? "Діва" : "Терези"; break;
+                    case 10: sign = (day <= 23) ? "Терези" : "Скорпіон"; break;
+                    case 11: sign = (day <= 22) ? "Скорпіон" : "Стрілець"; break;
+                    case 12: sign = (day <= 23) ? "Стрілець" : "Козеріг"; break;
+                    default: sign = ""; break;
+                }
+
+                return sign;
+            });
+
+            var zodiac2 = await Task<string>.Factory.StartNew(() =>
+            {
+                int ch_year = dateTimePicker1.Value.Year % 12;
+                string output;
+                switch (ch_year)
+                {
+                    case 0:  output = "Мавпа"; break;
+                    case 1: output = "Півень"; break;
+                    case 2: output = "Собака"; break;
+                    case 3: output = "Свиня"; break;
+                    case 4: output = "Пацюк"; break;
+                    case 5: output = "Бик"; break;
+                    case 6: output = "Тигр"; break;
+                    case 7: output = "Кролик"; break;
+                    case 8: output = "Дракон"; break;
+                    case 9: output = "Змія"; break;
+                    case 10: output = "Кінь"; break;
+                    case 11: output = "Коза"; break;
+                    default: output = ""; break;
+                }
+                return output;
+            });
+
+            textBox2.Text = zodiac1;
+            textBox3.Text = zodiac2;
+
+
+
+
+            dateTimePicker1.Enabled = true;
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
